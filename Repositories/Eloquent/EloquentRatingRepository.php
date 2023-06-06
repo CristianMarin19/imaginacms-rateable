254d92f28da59526613401aa8b254f3eed9744cd
Juan Carlos Escalante <jcec007@gmail.com>
2023-06-05 22:43:32 -0500

    +Update: added filterQuery params, updateBy and deleteBy optional params

--- Repositories/Eloquent/EloquentRatingRepository.php
+++ Repositories/Eloquent/EloquentRatingRepository.php
@@ -24,9 +24,10 @@ class EloquentRatingRepository extends EloquentCrudRepository implements RatingR
    *
    * @param $query
    * @param $filter
+   * @param $params
    * @return mixed
    */
-  public function filterQuery($query, $filter)
+  public function filterQuery($query, $filter, $params)
   {
 
     /**
